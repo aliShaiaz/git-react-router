4901c2e8833a746dad79536531a6dca7a896584b
alishaiaz <shaiazali@gmail.com>
2022-10-01 04:14:08 +0600

    first commit

---
+++ src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+const About = () => {
+    return (
+        <>
+            <div>
+                <h1>This is the ABOUT page!!</h1>
+            </div>
+        </>
+    );
+}
+
+export default About;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Home = () => {
+    return (
+        <>
+            <div>
+                <h1>This is the HOMEPAGE!</h1>
+            </div>
+        </>
+    );
+}
+
+export default Home;
